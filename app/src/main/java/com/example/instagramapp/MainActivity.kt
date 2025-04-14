c06827c2a31bbefe248dec99a4931d8cf44d7bff
Vihaan <vihaanverma21@gmail.com>
2025-04-15 00:18:30 +0530

    fixed permission crash

--- app/src/main/java/com/example/instagramapp/MainActivity.kt
+++ app/src/main/java/com/example/instagramapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.instagramapp
 
+import FeedViewModel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -17,23 +19,18 @@ import com.example.instagramapp.ui.theme.InstagramAppTheme
 import com.example.instagramclone.InstagramFeed
 
 class MainActivity : ComponentActivity() {
+    private val feedViewModel: FeedViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             InstagramAppTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    InstagramFeed()
+                    InstagramFeed(feedViewModel)
                 }
             }
         }
